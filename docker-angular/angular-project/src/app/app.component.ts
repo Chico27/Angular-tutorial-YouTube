d1888673122a36e1efeb9f9b478f3e1ae225bbee
Chico27 <masamasa.987654321@gmail.com>
2024-07-10 16:06:47 +0900

    補間による値の表示 #3

--- docker-angular/angular-project/src/app/app.component.ts
+++ docker-angular/angular-project/src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TestComponent } from './test/test.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TestComponent],
+  imports: [RouterOutlet, TestComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular-project';
+
+  testObject: Object = {
+    'a': 'test1',
+    'b': 'test2',
+    'c': 'test3',
+  };
+
+  testNum = 2 + 3;
+
+  itemImageUrl: String = 'assets/enhance_logo.PNG';
+
 }
