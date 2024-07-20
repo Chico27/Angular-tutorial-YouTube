275aa649e5dc2ca38f6e1ff190587800c54d6679
Chico27 <masamasa.987654321@gmail.com>
2024-07-20 13:11:22 +0900

    Angularのclassについてのチュートリアル #4

--- docker-angular/angular-project/src/app/test/test.component.ts
+++ docker-angular/angular-project/src/app/test/test.component.ts
@@ -9,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class TestComponent {
 
+  successClass: string = "success";
+  errorClass: string = "error";
+  hasError: boolean = true;
+
+  numericValue: string = "1";
+
+  checkClasses: Object = {
+    "success": this.isNumeric(this.numericValue),
+    "error": !this.isNumeric(this.numericValue),
+  } 
+  checkClassesList: Object = [
+    this.isNumeric(this.numericValue) ? "success" : "error",
+    "underline"
+  ] 
+
+  isNumeric(value: string): boolean {
+    // 正規表現を使って数値をチェックします。
+    return !isNaN(Number(value)) && value.trim() !== '';
+  }
 }
