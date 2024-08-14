f82354cb84b40e901c0c316ad28c1c23b23061df
Chico27 <masamasa.987654321@gmail.com>
2024-08-14 11:34:59 +0900

    コンポーネントライフサイクル #13

--- docker-angular/angular-project/src/app/app.component.ts
+++ docker-angular/angular-project/src/app/app.component.ts
@@ -2,26 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TestComponent } from './test/test.component';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TestComponent, CommonModule],
+  imports: [RouterOutlet, TestComponent, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title: string = 'angular-project';
-  num: number = 0.5;
-
-  testObject: Object = {
-    'a': 'test1',
-    'b': 'test2',
-    'c': 'test3',
-  };
-
-  upperTitle() {
-    return this.title.toUpperCase();
-  }
-
+  message: string = '';
 }
--- docker-angular/angular-project/src/app/test/test.component.ts
+++ docker-angular/angular-project/src/app/test/test.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,12 +8,29 @@ import { CommonModule } from '@angular/common';
   templateUrl: './test.component.html',
   styleUrl: './test.component.scss'
 })
-export class TestComponent {
+export class TestComponent implements OnInit, OnChanges, OnDestroy {
   @Input() parentData: string = '';
-  // @Input('parentData') test = '';
-  @Output() childEvent = new EventEmitter();
 
-  sendParentEvent() {
-    this.childEvent.emit('ボタンが押されました');
+  // コンポーネントの初期化時に実行
+  ngOnInit() {
+    console.log('ngOnInitが呼ばれました');
   }
+  
+  // ビューの初期化が完了し、すべてのテンプレートの描画が終了した後に実行
+  ngAfterViewInit() {
+    console.log('ngAfterViewInitが呼ばれました');
+  }
+  
+  // @Inputで入力値が設定されたときに実行
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChangesが呼ばれました');
+    console.log('現在のvalue：', changes['parentData'].currentValue);
+    console.log('以前のvalue：', changes['parentData'].previousValue);
+  }
+
+  // コンポーネントが破棄されるときに実行
+  ngOnDestroy() {
+    console.log('OnDestroyが呼ばれました');
+  }
+
 }
