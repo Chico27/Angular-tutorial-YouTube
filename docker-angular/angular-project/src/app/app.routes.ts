abb4783e386c97647ea160a9856f1f97b741ea0f
Chico27 <masamasa.987654321@gmail.com>
2024-09-28 22:05:54 +0900

    【Angular入門】ルーティングの基本 #17

--- docker-angular/angular-project/src/app/app.routes.ts
+++ docker-angular/angular-project/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { StoreListComponent } from './store-list/store-list.component';
+import { BookListComponent } from './book-list/book-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'stores',
+        component: StoreListComponent,
+        title: '店舗一覧'
+    },
+    {
+        path: 'books',
+        component: BookListComponent,
+        title: '書籍一覧'
+    },
+];
