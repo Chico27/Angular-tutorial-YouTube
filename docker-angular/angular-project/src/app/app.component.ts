0482c662e57fc8235d2189f90d4bab6efd524050
Chico27 <masamasa.987654321@gmail.com>
2024-10-05 21:43:11 +0900

    【Angular入門】ナビゲーションの基本 #18

--- docker-angular/angular-project/src/app/app.component.ts
+++ docker-angular/angular-project/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { BookListComponent } from './book-list/book-list.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BookListComponent, CommonModule, FormsModule],
+  imports: [RouterOutlet, BookListComponent, CommonModule, FormsModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
