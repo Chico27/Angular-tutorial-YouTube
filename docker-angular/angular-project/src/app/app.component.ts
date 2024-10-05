02ad0cfba0440c862d33d41712fdd9dc48669069
Chico27 <masamasa.987654321@gmail.com>
2024-10-06 07:46:49 +0900

    ナビゲーションの基本 #18

--- docker-angular/angular-project/src/app/app.component.ts
+++ docker-angular/angular-project/src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { BookListComponent } from './book-list/book-list.component';
-import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
